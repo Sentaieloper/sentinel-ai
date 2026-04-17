c320784aaaa09e55a45610578f5384055388a57c
Sentaieloper <sentinelai.team@outlook.com>
2026-04-17 14:43:12 +0200

    Implement Open And Close Leveraged Position Instructions

--- programs/sentinel/src/instructions/mod.rs
+++ programs/sentinel/src/instructions/mod.rs
@@ -3,9 +3,13 @@ pub mod subscribe;
 pub mod add_position;
 pub mod update_position;
 pub mod create_alert;
+pub mod open_leveraged;
+pub mod close_leveraged;
 
 pub use initialize_config::*;
 pub use subscribe::*;
 pub use add_position::*;
 pub use update_position::*;
 pub use create_alert::*;
+pub use open_leveraged::*;
+pub use close_leveraged::*;

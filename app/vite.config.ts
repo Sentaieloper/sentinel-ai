6cd3c560e760e0178a349ead2ccd1e8e64e4e474
Sentaieloper <sentinelai.team@outlook.com>
2026-04-17 13:00:01 +0000

    Configure Package Lock + Package + 3 More

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		'process.env': {},
+	},
+	resolve: {
+		alias: {
+			buffer: 'buffer',
+		},
+	},
+	optimizeDeps: {
+		include: ['buffer', '@coral-xyz/anchor', '@solana/web3.js'],
+	},
 	server: {
 		port: 5176,
 		proxy: {

f53aaa91d059a307108f61997b412ccf81ffb10b
Sentaieloper <sentinelai.team@outlook.com>
2026-05-02 15:23:27 +0000

    Configure .gitignore & Anchor & 2 More

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,17 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env': {},
-	},
-	resolve: {
-		alias: {
-			buffer: 'buffer',
-		},
-	},
-	optimizeDeps: {
-		include: ['buffer', '@coral-xyz/anchor', '@solana/web3.js'],
-	},
 	server: {
 		port: 5176,
 		proxy: {
